718636289682760858e6b4188a489883108b1539
Thomstrong <a80090@qq.com>
2018-09-15 19:04:45 +0800

    set up project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <h1>
+        Hello, world!
+        <App/>
+    </h1>,
+    document.getElementById('root')
+);
+registerServiceWorker();
