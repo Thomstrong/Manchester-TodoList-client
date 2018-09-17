ee07ac6d194cc67c93ecc5c0099f8e5bd5ee9b5b
Thomstrong <a80090@qq.com>
2018-09-17 23:06:35 +0800

    refine front-end logic and use fake data to test

--- src/App.js
+++ src/App.js
@@ -6,21 +6,39 @@ const cardColor = ["success-color", "primary-color", "warning-color", "danger-co
 const todoList = [
     {
         "id": "ef432037-5e41-4cca-8875-1441ec61cbe6",
-        "description": "hello",
+        "description": "\u521b\u5efa\u6846\u67b6",
         "deadline": "2018-09-16T13:00:00",
-        "restTime": "-1 day, 13:33:26.875040",
+        "restTime": "\u903e\u671f 2 \u5929 9 \u5c0f\u65f6 39 \u5206\u949f",
         "priority": 0,
         "status": 0,
-        "url": "http://127.0.0.1:8000/api/todoList/ef432037-5e41-4cca-8875-1441ec61cbe6/"
+        "url": "http://127.0.0.1:8005/api/todoList/ef432037-5e41-4cca-8875-1441ec61cbe6/"
     },
     {
         "id": "1b3d623f-3012-4edf-9f1c-ac7bdd6586eb",
         "description": "\u521b\u5efa\u6846\u67b6",
         "deadline": "2018-09-18T00:00:00",
-        "restTime": "1 day, 0:33:26.874559",
+        "restTime": "\u8fd8\u5269 0 \u5929 1 \u5c0f\u65f6 21 \u5206\u949f",
+        "priority": 1,
+        "status": 0,
+        "url": "http://127.0.0.1:8005/api/todoList/1b3d623f-3012-4edf-9f1c-ac7bdd6586eb/"
+    },
+    {
+        "id": "9e264176-f313-47e0-a0d8-fc48657ed20c",
+        "description": "\u524d\u540e\u7aef\u8054\u8c03",
+        "deadline": "2018-09-17T00:00:00",
+        "restTime": "\u903e\u671f 1 \u5929 22 \u5c0f\u65f6 39 \u5206\u949f",
         "priority": 2,
         "status": 0,
-        "url": "http://127.0.0.1:8000/api/todoList/1b3d623f-3012-4edf-9f1c-ac7bdd6586eb/"
+        "url": "http://127.0.0.1:8005/api/todoList/9e264176-f313-47e0-a0d8-fc48657ed20c/"
+    },
+    {
+        "id": "2aa264b5-bb31-4d63-a5d6-7facad0f7494",
+        "description": "\u524d\u540e\u7aef\u8054\u8c03\u5b8c\u6210",
+        "deadline": "2018-09-24T00:00:00",
+        "restTime": "\u8fd8\u5269 6 \u5929 1 \u5c0f\u65f6 21 \u5206\u949f",
+        "priority": 3,
+        "status": 0,
+        "url": "http://127.0.0.1:8005/api/todoList/2aa264b5-bb31-4d63-a5d6-7facad0f7494/"
     }
 ];
 
@@ -29,7 +47,9 @@ class App extends Component {
         super(props);
         this.state = {
             todoList: todoList,
-            show: false
+            newDialog: false,
+            modifyDialog: false
+
         };
 
     }
@@ -39,21 +59,42 @@ class App extends Component {
             this.setState({todoList: todoList});
         }
         else {
-            this.setState((prevState) => ({
-                    todoList: todoList.filter(item => item.description.includes(keyword))
-                })
-            );
+            this.setState({todoList: todoList.filter(item => item.description.includes(keyword))});
         }
     };
 
-    toggle = () => {
-        this.setState({
-            show: !this.state.show
-        });
+    toggle = (type) => {
+        if (type === "new") {
+            this.setState({
+                newDialog: !this.state.newDialog
+            })
+        } else {
+            this.setState({
+                modifyDialog: !this.state.modifyDialog
+            })
+        }
+
     };
-    handleFormData = (formData) => {
+    handleNewTask = () => {
+        this.setState({
+            newDialog: !this.state.newDialog
+        })
+    }
+    handleUpdateTask = (url) => {
+        this.setState({
+            modifyDialog: !this.state.modifyDialog
+        })
+    }
+
+    handleFormData = (formData, method) => {
         console.log(formData);
-        this.toggle();
+        let deadline = formData.deadline.format("YYYY-MM-DD hh:mm:ss");
+        console.log(deadline);
+        if (method === "POST") {
+            this.toggle("new")
+        } else {
+            this.toggle("modify")
+        }
     };
 
     handleSearch = (event) => {
@@ -86,7 +127,12 @@ class App extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <NewTaskForm title={"新建待办事项"} show={this.state.show} handleSubmit={this.handleFormData}/>
+                    <NewTaskForm onClose={() => this.toggle("new")} title={"新建待办事项"} show={this.state.newDialog}
+                                 handleSubmit={(data) => this.handleFormData(data, "POST")}/>
+                </div>
+                <div className="row">
+                    <NewTaskForm onClose={() => this.toggle("modify")} title={"修改待办事项"} show={this.state.modifyDialog}
+                                 handleSubmit={(data) => this.handleFormData(data, "PUT")}/>
                 </div>
                 <div className="col-lg-9 ml-auto mr-auto">
                     <div className="row position-relative w-auto justify-content-md-center">
@@ -101,7 +147,7 @@ class App extends Component {
                                 </button>
                             </div>
                             <div className="col-lg-4 mt-auto mb-auto">
-                                <button type="button" onClick={this.toggle} className={"btn btn-danger btn-sm"}>
+                                <button type="button" onClick={this.handleNewTask} className={"btn btn-danger btn-sm"}>
                                     新建
                                 </button>
                             </div>
@@ -118,9 +164,14 @@ class App extends Component {
                                 <div className="card-body">
                                     <h4 className="card-title">Special title treatment</h4>
                                     <p className="card-text">{item.deadline}</p>
+                                    <p className="card-text">{item.restTime}</p>
                                     <button onClick={() => this.deleteTask(item.url)} type="button"
                                             className={"btn btn-sm " + color}>
                                         删除
+                                    </button>
+                                    <button onClick={() => this.handleUpdateTask(item.url)} type="button"
+                                            className={"btn btn-sm " + color}>
+                                        修改
                                     </button>
                                 </div>
                             </div>
--- src/Components/NewTaskForm.jsx
+++ src/Components/NewTaskForm.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 import {Input} from "mdbreact"
+import "react-datetime/css/react-datetime.css"
+import Datetime from "react-datetime"
 
 class NewTaskForm extends Component {
     constructor(props) {
@@ -15,6 +17,12 @@ class NewTaskForm extends Component {
         event.preventDefault();
     }
 
+    consumeInput = (props) => {
+        return (
+            <Input label="截止日期" {...props} onChange={(event) => props.onChange(event.target.value)}/>
+        );
+    }
+
     render() {
         return (
             <Modal isOpen={this.props.show}>
@@ -22,28 +30,29 @@ class NewTaskForm extends Component {
                     <ModalHeader>{this.props.title}</ModalHeader>
                     <ModalBody>
                         <div className="row">
-                            <div className="form-group col-md-4">
+                            <div className="form-group col-lg-12">
                                 <Input label={"待办事项标题"}
                                        onChange={(event) => this.setState({discription: event.target.value})}/>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="form-group col-md-4">
-                                <Input label={"截止日期"}
-                                       onChange={(event) => this.setState({deadline: event.target.value})}/>
+                            <div className="form-group col-lg-12">
+                                <Datetime renderInput={this.consumeInput}
+                                          onChange={(value) => this.setState({deadline: value})}/>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="form-group col-md-4">
+                            <div className="form-group col-lg-12">
                                 <label>Country:</label>
                                 <input type="text" value={this.country} onChange={this.handleChangeCountry}
                                        className="form-control"/>
                             </div>
                         </div>
+
                     </ModalBody>
                     <ModalFooter>
-                        <input type="submit" value="Submit" color="primary" className="btn btn-primary"/>
-                        <Button color="danger" onClick={this.toggle}>Cancel</Button>
+                        <input type="submit" value="提交" color="primary" className="btn btn-primary"/>
+                        <Button color="danger" onClick={this.props.onClose}>取消</Button>
                     </ModalFooter>
                 </form>
             </Modal>
