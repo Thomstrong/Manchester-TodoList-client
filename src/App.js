fe64a306402648d78d6faddf335af63c63060fa1
Thomstrong <a80090@qq.com>
2018-09-17 11:06:48 +0800

    improve style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import SearchBox from "./Components/SearchBox";
-import Panel from "react-bootstrap/es/Panel";
 
+const cardColor = {
+    0: "success-color",
+    1: "primary-color",
+    2: "warning-color",
+    3: "danger-color"
+}
 const todoList = [
     {
         "id": "ef432037-5e41-4cca-8875-1441ec61cbe6",
@@ -17,7 +22,7 @@ const todoList = [
         "description": "\u521b\u5efa\u6846\u67b6",
         "deadline": "2018-09-18T00:00:00",
         "restTime": "1 day, 0:33:26.874559",
-        "priority": 4,
+        "priority": 2,
         "status": 0,
         "url": "http://127.0.0.1:8000/api/todoList/1b3d623f-3012-4edf-9f1c-ac7bdd6586eb/"
     }
@@ -42,37 +47,45 @@ class App extends Component {
 
     render() {
         const todoList = this.state.todoList;
-        console.log(todoList)
         return (
-            <div>
-                <div className="container">
-                    <div className="col-lg-8 ml-auto mr-auto">
-                        <div className="row">
-                            <div className="col-md-8 mt-auto mb-auto">
-                                <SearchBox label={"查询代办事项"}/>
-                            </div>
-                            <div className="col-md-4 mt-auto mb-auto">
-                                <button type="button" className={"btn btn-danger btn-sm"}>+
-                                </button>
-                            </div>
+            <div className="container">
+                <div className="col-lg-8 ml-auto mr-auto">
+                    <div className="row">
+                        <div className="col-md-8 mt-auto mb-auto">
+                            <SearchBox label={"查询代办事项"}/>
+                        </div>
+                        <div className="col-md-2 mt-auto mb-auto">
+                            <button type="button" className={"btn btn-danger btn-sm"}>
+                                查询
+                            </button>
+                        </div>
+                        <div className="col-md-2 mt-auto mb-auto">
+                            <button type="button" onClick={() => {
+                                this.setState({open: true})
+                            }} className={"btn btn-danger btn-sm"}>
+                                新建
+                            </button>
                         </div>
                     </div>
-                    <div className="col-lg-8 ml-auto mr-auto">
-                        {todoList.map((item) => {
-                            return (<div bsStyle="primary" class="h-auto card card-body">
-                                <Panel.Heading>
-                                    <Panel.Title componentClass="h3">{item.description}</Panel.Title>
-                                </Panel.Heading>
-                                <Panel.Body>{item.deadline}</Panel.Body>
-                                <Panel.Body>
+                </div>
+                <div className="col-lg-8 ml-auto mr-auto">
+                    {todoList.map((item) => {
+                        let color = cardColor[item.priority].toString();
+                        return (
+                            <div className="card">
+                                <div
+                                    className={"card-header lighten-1 white-text " + color}>{item.description}</div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Special title treatment</h4>
+                                    <p className="card-text">{item.deadline}</p>
                                     <button onClick={() => this.deleteTask(item.url)} type="button"
-                                            className={"btn btn-danger btn-sm"}>
+                                            className={"btn btn-sm " + color}>
                                         删除
                                     </button>
-                                </Panel.Body>
-                            </div>)
-                        })}
-                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
